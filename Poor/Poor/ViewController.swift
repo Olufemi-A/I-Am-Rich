74a3649c9ef9944048a118c6f7e1e0af0b5aed62
Olufemi-A <omotayoadefila@gmail.com>
2020-04-15 00:22:57 -0400

    Added another version called poor

---
+++ Poor/Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Poor
+//
+//  Created by Olufemi Adefila on 4/14/20.
+//  Copyright © 2020 Olufemi Adefila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
